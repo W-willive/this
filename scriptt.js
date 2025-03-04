8cf6d800ea92aa89afec7ce87a4aeb5c1a8822a4
W-willive <willivesaturai@gmail.com>
2025-03-04 19:11:48 +0300

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,19 +1,19 @@
-// script.js
+
 function changeText() {
   const welcomeText = document.getElementById("welcome-text");
   const button = document.getElementById("change-text-btn");
   
-  // Change text content
+ 
   welcomeText.innerText = "JavaScript is Dynamic and Fun!";
   
-  // Add a pulsating animation
+ 
   welcomeText.classList.add("pulse-animation");
   
-  // Change button color randomly
+  
   button.style.backgroundColor = getRandomColor();
 }
 
-// Function to generate a random color
+
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
